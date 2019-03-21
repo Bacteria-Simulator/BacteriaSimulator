21216989864981e5ea5d12589c8b9fe0119cdd5b
Bacteria-Simulator <47617093+Bacteria-Simulator@users.noreply.github.com>
2019-03-20 20:59:51 -0400

    updated legend with clear information

--- resources/views/simulations.blade.php
+++ resources/views/simulations.blade.php
@@ -435,6 +435,7 @@
 </br>
 <center>
     <h3 id="path_name"></h3>
+    <label id="legend_info">Cells per Hexagon:</label>
     <label id="num_cells">Number of Cells: 0</label>
     <label id="lot" class="col-md-offset-1">Length of Time (Minutes): 0</label>
     <label class="col-md-offset-1" for="link">More Info: </label><a id="link" href=""></a>
