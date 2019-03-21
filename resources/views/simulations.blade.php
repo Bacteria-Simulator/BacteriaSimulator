cd93b93f92512ae4f302da029b9a1e803d36e7e0
Bacteria-Simulator <47617093+Bacteria-Simulator@users.noreply.github.com>
2019-03-20 22:05:29 -0400

    Update simulations.blade.php

--- resources/views/simulations.blade.php
+++ resources/views/simulations.blade.php
@@ -226,8 +226,7 @@ svg.append("g")
 var legend = d3.legendColor()
   .labelFormat(d3.format(".2f"))
   .useClass(true)
-  .title("A really really really really 
-    really long title")
+  .title("A ")
   .titleWidth(100)
   .scale(quantize);
 
