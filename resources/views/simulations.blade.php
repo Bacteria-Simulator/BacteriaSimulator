78a65de54c7831cf469cbfd9c17578b652148c8f
Bacteria-Simulator <47617093+Bacteria-Simulator@users.noreply.github.com>
2019-03-22 22:39:19 -0400

    Update simulations.blade.php

--- resources/views/simulations.blade.php
+++ resources/views/simulations.blade.php
@@ -242,7 +242,7 @@
                         if(lot % 1 != 0)
                             lot++;
                         else{
-                            minutes+= minutes*100;
+                            minutes++;
                             lot = 1;
                             $("#lot").html("Time Elapsed (Minutes) " + msg + ": " + minutes);
                         }
